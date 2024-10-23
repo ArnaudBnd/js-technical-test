75a16230f9444e57625ec99121724d2e6d70390b
arnaudbnd <arnaud.benede.contact@gmail.com>
2024-10-23 21:52:00 +0200

    Update Docker and knex configuration to use environment variables for database connection

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -5,10 +5,10 @@ import path from 'path';
 const config = {
   client: 'mysql2',
   connection: {
-    host: 'localhost',
-    user: 'eleven',
-    password: 'eleven11',
-    database: 'eleven',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'eleven',
+    password: process.env.DB_PASSWORD || 'eleven11',
+    database: process.env.DB_NAME || 'eleven',
   },
   migrations: {
     tableName: 'knex_migrations',
